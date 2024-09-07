3c9ef3f858305bb8d9256f6f38a1471e848ac9b3
Nimira43 <n.rabone72@aol.com>
2024-09-07 02:14:29 +0100

    Setting up Poppins font using Fontsource, creating colour variables and starting on base styles.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <h1>Svelte Tunez</h1>
 
+<input />
+<select>
+  <option>Option</option>
+</select>
+<button>Button</button>
+
+
 <style lang="scss">
   
   h1 {
-    background-color: #111;
     color: #ff4500;
     font-size: functions.toRem(36);
     @include breakpoint.up('md') {
