ce414376cdae67d67a26b280c0ed0f789392f617
Nimira43 <n.rabone72@aol.com>
2024-09-07 04:49:58 +0100

    Working on auth/login and auth/callback but getting redirect errors

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -1,4 +1,6 @@
-import { redirect, type RequestHandler } from '@sveltejs/kit'
+import { redirect } from '@sveltejs/kit'
+import pkce from 'pkce-gen'
+import type { RequestHandler } from './$types'
 import { SPOTIFY_APP_CLIENT_ID, BASE_URL } from '$env/static/private'
 
 const generateRandomString = (length: number) => {
@@ -11,19 +13,24 @@ const generateRandomString = (length: number) => {
   return randomString
 }
 
-const scope = 'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read'
+
 
 const state = generateRandomString(16)
+const challenge = pkce.create()
+
+const scope = 'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read'
 
 export const GET: RequestHandler = () => {
-  throw redirect(307, `https://accounts.spotify.com/authorize?${
-    new URLSearchParams({
+  throw redirect(
+    307,
+    `https://accounts.spotify.com/authorize?${new URLSearchParams({
       response_type: 'code',
       client_id: SPOTIFY_APP_CLIENT_ID,
       scope,
       redirect_url: `${BASE_URL}/api/auth/callback`,
       state,
       code_challenge_method: 'S256',
-    })
-  }`)
+      code_challenge: challenge.code_challenge
+    })}`
+  )
 }
