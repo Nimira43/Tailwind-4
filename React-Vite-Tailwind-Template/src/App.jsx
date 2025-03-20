e6ac7a9f27214e2162c0b49271de36c72563848f
Nimira43 <n.rabone72@aol.com>
2025-03-20 01:31:06 +0000

    Added colour variables.

--- React-Vite-Tailwind-Template/src/App.jsx
+++ React-Vite-Tailwind-Template/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className='bg-black text-orange-500 text-4xl p-10'>
+    <div className='bg-prime text-white text-4xl p-10'>
       <h1>Tailwind React Stater</h1>
     </div>
   )
