2ed9e86d778c7fabcc9399da99e619e12c8cfc73
Nimira43 <n.rabone72@aol.com>
2025-04-18 05:14:50 +0100

    Border colours are now set by text colour in TW4

--- Tailwind-Templates/vite-boilerplate/src/App.jsx
+++ Tailwind-Templates/vite-boilerplate/src/App.jsx
@@ -18,6 +18,11 @@ const App = () => {
       <div className='bg-prime container'>
         <h1 className='text-gold text-2xl'>Vite - Tailwind - React Boilerplate </h1>
       </div>
+      <div className='bg-support-dark p-20'>
+        <button className='bg-dark text-prime p-5 border-4 rounded cursor-pointer m-3'>Button</button>
+        <button className='bg-prime text-light p-5 border-4 rounded cursor-pointer m-3'>Button</button>
+        <button className='bg-grey-light text-support p-5 border-4 rounded cursor-pointer m-3'>Button</button>
+      </div>
     </>
     
   )
