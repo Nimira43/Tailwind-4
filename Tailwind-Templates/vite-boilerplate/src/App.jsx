7541b926203c262158c121f5574b74dd6ccf35d9
Nimira43 <n.rabone72@aol.com>
2025-04-18 05:35:11 +0100

    Placeholder text adopts the text colour too, with a reduced opacity.

--- Tailwind-Templates/vite-boilerplate/src/App.jsx
+++ Tailwind-Templates/vite-boilerplate/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <>
+      
       <div className='container py-10 bg-grey-light'>
         <div className='bg-dark'>
           <h1 className='text-light text-2xl'>Vite - Tailwind - React Boilerplate </h1>
@@ -20,18 +21,36 @@ const App = () => {
           <h1 className='text-gold text-2xl'>Vite - Tailwind - React Boilerplate </h1>
         </div>
       </div>
+
       <div className='bg-support-dark p-10 container'>
         <button className='bg-dark text-prime p-5 border-4 rounded cursor-pointer m-3'>Button</button>
         <button className='bg-prime text-light p-5 border-4 rounded cursor-pointer m-3'>Button</button>
         <button className='bg-grey-light text-support p-5 border-4 rounded cursor-pointer m-3'>Button</button>
       </div>
+
       <div className='bg-grey-dark p-10 container'>
         <button className='bg-dark text-gold p-5 rounded cursor-pointer m-3'>Button</button>
         <button className='bg-prime text-light p-5 rounded cursor-pointer m-3'>Button</button>
         <button className='bg-grey-light text-dark p-5 rounded cursor-pointer m-3'>Button</button>
       </div>
-    </>
+
+      <div className='container bg-support-light p-10'>
+        <input
+          type="text" placeholder='Name'
+          className='block bg-dark text-prime py-2 px-3 border-0 outline-0 rounded-md mb-3' 
+        />
+        <input
+          type="text" placeholder='Email'
+          className='block bg-dark text-prime py-2 px-3 border-0 outline-0 rounded-md mb-3' 
+        />
+        <input
+          type="text" placeholder='Password'
+          className='block bg-dark text-prime py-2 px-3 border-0 outline-0 rounded-md mb-3' 
+        />
+
+      </div>
     
+    </>
   )
 }
 
