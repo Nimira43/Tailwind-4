b9046c6094b5d01c1b06370139aeeed7d4cb181f
Nimira43 <n.rabone72@aol.com>
2025-04-18 04:16:15 +0100

    Setting up theme colour variables.

--- Tailwind-Templates/vite-boilerplate/src/App.jsx
+++ Tailwind-Templates/vite-boilerplate/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-orange-500 p-20'>
-      <h1 className='text-white text-5xl'>Vite - Tailwind - React Boilerplate </h1>
+    <div className='bg-black p-20'>
+      <h1 className='text-prime text-5xl'>Vite - Tailwind - React Boilerplate </h1>
     </div>
   )
 }
