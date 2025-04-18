1b29cb447b98541d13c1d2f62ddaa5c85674561f
Nimira43 <n.rabone72@aol.com>
2025-04-18 05:01:47 +0100

    Created container utlity

--- Tailwind-Templates/vite-boilerplate/src/App.jsx
+++ Tailwind-Templates/vite-boilerplate/src/App.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <div className='bg-dark p-5'>
+      <div className='bg-dark container'>
         <h1 className='text-light text-2xl'>Vite - Tailwind - React Boilerplate </h1>
       </div>
-      <div className='bg-support-dark p-5'>
+      <div className='bg-support-dark container'>
         <h1 className='text-support-light text-2xl'>Vite - Tailwind - React Boilerplate </h1>
       </div>
-      <div className='bg-grey-dark p-5'>
+      <div className='bg-grey-dark container'>
         <h1 className='text-grey-light-extra text-2xl'>Vite - Tailwind - React Boilerplate </h1>
       </div>
-      <div className='bg-support p-5'>
+      <div className='bg-support container'>
         <h1 className='text-grey-light text-2xl'>Vite - Tailwind - React Boilerplate </h1>
       </div>
-      <div className='bg-prime p-5'>
+      <div className='bg-prime container'>
         <h1 className='text-gold text-2xl'>Vite - Tailwind - React Boilerplate </h1>
       </div>
     </>
