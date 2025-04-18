7cac28d566fb1c132ee56e4ff739abe7910ac9b1
Nimira43 <n.rabone72@aol.com>
2025-04-18 04:31:07 +0100

    Testing custom colours

--- Tailwind-Templates/vite-boilerplate/src/App.jsx
+++ Tailwind-Templates/vite-boilerplate/src/App.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-black p-20'>
-      <h1 className='text-prime text-5xl'>Vite - Tailwind - React Boilerplate </h1>
-    </div>
+    <>
+      <div className='bg-dark p-5'>
+        <h1 className='text-light text-2xl'>Vite - Tailwind - React Boilerplate </h1>
+      </div>
+      <div className='bg-support-dark p-5'>
+        <h1 className='text-support-light text-2xl'>Vite - Tailwind - React Boilerplate </h1>
+      </div>
+      <div className='bg-grey-dark p-5'>
+        <h1 className='text-grey-light-extra text-2xl'>Vite - Tailwind - React Boilerplate </h1>
+      </div>
+      <div className='bg-support p-5'>
+        <h1 className='text-grey-light text-2xl'>Vite - Tailwind - React Boilerplate </h1>
+      </div>
+      <div className='bg-prime p-5'>
+        <h1 className='text-gold text-2xl'>Vite - Tailwind - React Boilerplate </h1>
+      </div>
+    </>
+    
   )
 }
 
