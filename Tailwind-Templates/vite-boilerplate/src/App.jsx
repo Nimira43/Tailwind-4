a1d3f381ded070baca353e0fa1a172598cda7fe8
Nimira43 <n.rabone72@aol.com>
2025-04-18 04:02:28 +0100

    Testing TW - all good

--- Tailwind-Templates/vite-boilerplate/src/App.jsx
+++ Tailwind-Templates/vite-boilerplate/src/App.jsx
@@ -1,5 +1,12 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <div>Vite - Tailwind Template</div>
+    <div className='bg-orange-500 p-20'>
+      <h1 className='text-white text-5xl'>Vite - Tailwind - React Boilerplate </h1>
+    </div>
   )
 }
+
+export default App
+
